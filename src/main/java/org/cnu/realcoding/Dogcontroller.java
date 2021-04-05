3150b17451992adbbfd1b87ee3baee8da3b200e8
kyun <kyun1229@naver.com>
2021-04-05 15:45:59 +0900

    Initial commit

---
+++ src/main/java/org/cnu/realcoding/Dogcontroller.java
@@ -0,0 +1,8 @@
+package org.cnu.realcoding;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Dogcontroller {
+}
---
+++ src/main/java/org/cnu/realcoding/MyFirstSpringBootProject16Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootProject16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootProject16Application.class, args);
+	}
+
+}
